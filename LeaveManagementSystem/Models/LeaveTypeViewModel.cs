1ca858ca2dab7411620496d68caba7ad12fd2b31
Krystian Lis <krystian.lis@intel.com>
2020-02-15 22:26:54 +0100

    add view models

--- LeaveManagementSystem/Models/LeaveTypeViewModel.cs
+++ LeaveManagementSystem/Models/LeaveTypeViewModel.cs
@@ -4,13 +4,13 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LeaveManagementSystem.Data
+namespace LeaveManagementSystem.Models
 {
-    public class LeaveType
+    public class LeaveTypeViewModel
     {
-        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public DateTime DateCreated { get; set; }
